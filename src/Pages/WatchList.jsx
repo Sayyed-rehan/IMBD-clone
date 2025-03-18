74094a28d305c7304d18a0fa87b8ce4aab3d452b
Sayyed-rehan <rehansayyed1216@gmail.com>
2025-03-18 15:27:49 +0530

    sorting functionality implemented

--- src/Pages/WatchList.jsx
+++ src/Pages/WatchList.jsx
@@ -1,22 +1,65 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import genreids from "../Uitility";
 
 const WatchList = () => {
-  let watchlist_data = JSON.parse(localStorage.getItem("watchlist")) || [];
-  console.log(watchlist_data);
+
+
+  const [watchList, setwatchList] = useState([])
+
+  const handleLow = ()=>{
+    console.log('low');
+
+    watchList.sort((ob1, ob2)=>{
+      return ob1.vote_average - ob2.vote_average
+    })
+    console.log('from low',watchList);
+    setwatchList([...watchList])
+  }
+
+  const handleHigh = ()=>{
+    console.log('high');
+ 
+    watchList.sort((ob1, ob2)=>{
+      return ob2.vote_average - ob1.vote_average
+    })
+    console.log('from high',watchList);
+    setwatchList([...watchList])
+
+  }
+  
+
+  const handleNormal = ()=>{
+    let watchlist_data = JSON.parse(localStorage.getItem("watchlist")) || []
+
+    setwatchList([...watchlist_data])
+  }
+
+  useEffect(()=>{
+    let watchlist_data = JSON.parse(localStorage.getItem("watchlist")) || []
+    console.log(watchlist_data);
+    setwatchList([...watchlist_data])
+  },[])
+
   return (
     <div className="overflow-hidden rounded-lg border border-gray-200 shadow-md m-5">
       <table className="w-full border-collapse bg-white text-left text-sm text-gray-500">
         <thead>
           <tr className="bg-gray-50">
             <th className="px-6 py-4 font-medium text-gray-900">Name</th>
-            <th>Ratings</th>
+            <th className="ml-[20px]">
+            <div className="flex items-center gap-[10px]">
+            <i class="fa-solid fa-arrow-up" onClick={handleHigh}  style={{'font-size': 'large', 'cursor':"pointer"}}></i>
+            <div className="cursor-pointer" onClick={handleNormal}>Ratings</div>
+            <i class="fa-solid fa-arrow-down" onClick={handleLow} style={{'font-size': 'large', 'cursor':"pointer"}}></i>
+            </div>
+            </th>
             <th>Popularity</th>
             <th>Genre</th>
           </tr>
         </thead>
         <tbody className="divide-y divide-gray-100 border-t border-gray-100">
-          {watchlist_data.length > 0 ? (
-            watchlist_data.map((item, index) => {
+          {watchList.length > 0 ? (
+            watchList.map((item, index) => {
               return (
                 <tr className="hover:bg-gray-100" key={item.id}>
                   <td className="flex items-center gap-[50px] px-6 py-4 font-normal text-gray-900">
@@ -32,12 +75,14 @@ const WatchList = () => {
 
                   <td className="pl-6 py-4">{item.vote_average}</td>
                   <td className="pl-6 py-4">{item.popularity}</td>
-                  <td className="pl-2 py-4">{item.genre_ids[0]}</td>
+                  <td className="pl-2 py-4">
+                    {genreids[item.genre_ids[0]] || 'NA'}
+                  </td>
                 </tr>
               );
             })
           ) : (
-            <h1>No Data Available</h1>
+            <h1 className="font-medium text-gray-700 text-xl">No Data Available</h1>
           )}
         </tbody>
       </table>
