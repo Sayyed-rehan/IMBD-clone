cbdfa49cf479f3ca45a046c63e799851c001b070
Sayyed-rehan <rehansayyed1216@gmail.com>
2025-03-21 11:33:10 +0530

    IMDb-logo path modified

--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -1,12 +1,15 @@
 import React from "react";
 import { Link } from "react-router-dom";
-import imdb_logo from "../../public/imdblogo.png"
+
+
 
 const Navbar = () => {
+
+  let path = '../../imdbLogo.png'
   return (
     <div className="flex space-x-8 items-center pl-3 py-4">
       <Link to="/" className="">
-        <img src={imdb_logo} alt="imdb-logo"  className="w-[70px]"/>
+        <img src={path} alt="imdb-logo"  className="w-[70px]"/>
       </Link>
       <Link className="text-blue-500 text-3xl font-bold" to="/">Movies</Link>
       <Link className="text-blue-500 text-3xl font-bold" to="/watchlist">
